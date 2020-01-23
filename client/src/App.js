0d3c067c30ddc333ff75ce396c924bc27d157ea7
Ikuseghan Pryce <yardgal21@yahoo.com>
2020-01-23 10:10:02 -0500

    Updates to the client folder adding the chart graphs

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,61 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import Chart from './Anna/charts';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+class App extends Component {
+    constructor() {
+        super();
+        this.state = {
+            chartData: {}
+
+        }
+    }
+
+    componentWillMount() {
+        this.getChartData();
+    }
+
+    getChartData() {
+        //Ajax calls here
+        this.setState({
+            chartData: {
+                labels: ['Boston', 'Worcester', 'Springfield', 
+                'Lowell', 'Cambridge', 'New Bedford'],
+                datasets: [
+                    {
+                        label: 'Population',
+                        data:[
+                            617594,
+                            181045,
+                            153060,
+                            106519,
+                            105162,
+                            95072
+                        ],
+                        backgroundColor:[
+                            'rgba(255, 99, 132, 0.6)',
+                            'rgba(54, 162, 235, 0.6)',
+                            'rgba(255, 206, 86, 0.6)',
+                            'rgba(75, 192, 192, 0.6)',
+                            'rgba(153, 102, 255, 0.6)',
+                            'rgba(255, 159, 64, 0.6)',
+                            'rgba(255, 99, 132, 0.6)'
+                        ]
+                    }
+                ]
+            
+            }
+        });
+    }
+    render() {
+        return (
+            <div className="App">
+              <Chart chartData={this.state.chartData} location=" New York" legendPosition="bottom"/>
+            </div>
+          );
+        }
+
+    }
+  
 
 export default App;
--- client/src/index.js
+++ client/src/index.js
@@ -2,9 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import Chart from './Anna/charts';
+// import Anna from './Anna/anna';
+// import Graphs from './Anna/graphs';
+// import RenderLineChart from './Anna/graphs';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
+//ReactDOM.render(<Anna />, document.getElementById('root'));
+// ReactDOM.render(<Graphs />, document.getElementById('root'));
+
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
